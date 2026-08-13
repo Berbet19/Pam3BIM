c3c51d9f0f7f8bd6324e691751c4588250b0aa6b
Berbet <juliatrindadeberbet@gmail.com>
2026-08-13 19:00:51 -0300

    feat: initial commit

---
+++ app/(tabs)/conversas.tsx
@@ -0,0 +1,12 @@
+import { ButtonComponent } from '@/components/Button'
+import { Text, View } from 'react-native'
+
+export default function Conversas(){
+    return(
+        <View>
+            <Text>Conversas</Text>
+
+            <ButtonComponent name='botão' color='white'/>
+        </View>
+    )
+}
---
+++ data/product.ts
@@ -0,0 +1,8 @@
+export const products = [
+  { id: "1", name: "teste", description: "produtinnnn 1"  , price : 43.99, image:  require('../assets/images/camisa.jpg') },
+  { id: "2", name: "teste 2", description: "produtinnnn 2" },
+  { id: "3", name: "teste 3", description: "produtinnnn 3" },
+  { id: "4", name: "teste 4", description: "produtinnnn 4" },
+  { id: "5", name: "teste 5", description: "produtinnnn 5" },
+ 
+]
